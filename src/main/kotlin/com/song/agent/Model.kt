d01ec70e7bf89675df6424c88a7531ba641b9484
MHchangjun <opopgta@gmail.com>
2026-04-03 14:55:02 +0900

    clear

--- src/main/kotlin/com/song/agent/Model.kt
+++ src/main/kotlin/com/song/agent/Model.kt
@@ -5,42 +5,6 @@ import ai.koog.prompt.llm.LLMProvider
 import ai.koog.prompt.llm.LLModel
 
 object Model {
-    val QWEN = LLModel(
-        provider = LLMProvider.Ollama,
-        id = "qwen3-coder:30b",
-        capabilities = listOf(
-            LLMCapability.Temperature,
-            LLMCapability.Schema.JSON.Basic,
-            LLMCapability.Tools
-        ),
-        contextLength = 262_144,
-    )
-
-
-    val QWEN_LLAMA = LLModel(
-        provider = LLMProvider.OpenAI,
-        id = "qwen3-next",
-        capabilities = listOf(
-            LLMCapability.Temperature,
-            LLMCapability.Schema.JSON.Basic,
-            LLMCapability.Tools,
-            LLMCapability.OpenAIEndpoint.Completions,
-            LLMCapability.Completion
-        ),
-        contextLength = 262_144,
-    )
-
-    val QWEN_3_5 = LLModel(
-        provider = LLMProvider.Ollama,
-        id = "qwen3.5:35b",
-        capabilities = listOf(
-            LLMCapability.Temperature,
-            LLMCapability.Schema.JSON.Basic,
-            LLMCapability.Tools
-        ),
-        contextLength = 262_144,
-    )
-
     val QWEN_3_5_LLAMA = LLModel(
         provider = LLMProvider.OpenAI,
         id = "qwen3.5",
